5a225465a4a7a64a637713870c0fad04065c6ec1
quanglm <minhquang130698@gmail.com>
2024-10-08 15:58:50 +0700

    fix error

--- model/build.sh
+++ model/build.sh
@@ -199,49 +199,21 @@ download_file 'https://huggingface.co/ybelkada/flan-t5-xl-sharded-bf16/resolve/m
 download_file 'https://huggingface.co/ybelkada/flan-t5-xl-sharded-bf16/resolve/main/tokenizer_config.json?download=true' "${TARGET_DIR}/t5_model/flan-t5-xl-sharded-bf16/tokenizer_config.json"wnload_file 'https://civitai.com/api/download/models/274039' "${TARGET_DIR}/checkpoints/juggernaut_reborn.safetensors"
 
 
-echo "Checking out custom nodes..."
-checkout_gitrepo https://github.com/Fannovel16/comfyui_controlnet_aux.git "14c4feda42e218cf026d699d8d6c65917d73aa61" comfyui_controlnet_aux ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/cubiq/ComfyUI_IPAdapter_plus.git "4b6d3c3518258e543af75a1731084f2db3157f70" ComfyUI_IPAdapter_plus ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/lienminhquang/was-node-suite-comfyui.git "0b8469bd33d0ab6f6a5840c136612ed572f43522" was-node-suite-comfyui ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/Nourepide/ComfyUI-Allor.git "b7fb9ff0bd50124afcbf8cd2638a73f883d32a23" ComfyUI-Allor ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/lienminhquang/ComfyUI-Impact-Pack.git "869a43f00ed6c3867a7cdd0d8a8a2ebe75b9b5a3" ComfyUI-Impact-Pack ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/Gourieff/comfyui-reactor-node.git "078c7e6a17a3998ce81e71d7e7d14e61999bc5e1" comfyui-reactor-node ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/jags111/efficiency-nodes-comfyui.git "c15913587f218ec17ac4208d382a789934929ae4" efficiency-nodes-comfyui ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/ceruleandeep/ComfyUI-LLaVA-Captioner.git "8fcd56888ca9eb13d7dd91ab0e6431ebc2ccfc9c" ComfyUI-LLaVA-Captioner ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git "34bf73213d444f8270b6547eeb0d5982ce7ae50e" ComfyUI-Inspire-Pack ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/laksjdjf/attention-couple-ComfyUI.git "818b4a2af177d9578e86a5dbe5cab433be6f961c" attention-couple-ComfyUI ${TARGET_DIR}/custom_nodes
+checkout_gitrepo https://github.com/Fannovel16/comfyui_controlnet_aux.git "14c4feda42e218cf026d699d8d6c65917d73aa61" comfyui_controlnet_aux "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/cubiq/ComfyUI_IPAdapter_plus.git "4b6d3c3518258e543af75a1731084f2db3157f70" ComfyUI_IPAdapter_plus "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/lienminhquang/was-node-suite-comfyui.git "e7a74db7916b183981c358911557d5f55893b849" was-node-suite-comfyui "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/Nourepide/ComfyUI-Allor.git "b7fb9ff0bd50124afcbf8cd2638a73f883d32a23" ComfyUI-Allor "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/lienminhquang/ComfyUI-Impact-Pack.git "f6a5d0c02f7c9a6dec4ef95220112e85b73bbb00" ComfyUI-Impact-Pack "${TARGET_DIR}/custom_nodes"
+# checkout_gitrepo https://github.com/PurrfectAI/comfyUI-custom-nodes.git "5cb3c6dcff47017b006f8f09312a0c1cbeaef15f" comfyUI-custom-nodes "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/Gourieff/comfyui-reactor-node.git "161585495271c14fdf4b299f93e4c4fe5be1d2cc" comfyui-reactor-node "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/jags111/efficiency-nodes-comfyui.git "a6df20f4c60af49f08bbe122bc486c286f5d0745" efficiency-nodes-comfyui "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/ceruleandeep/ComfyUI-LLaVA-Captioner.git "8fcd56888ca9eb13d7dd91ab0e6431ebc2ccfc9c" ComfyUI-LLaVA-Captioner "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git "34bf73213d444f8270b6547eeb0d5982ce7ae50e" ComfyUI-Inspire-Pack "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/laksjdjf/attention-couple-ComfyUI.git "818b4a2af177d9578e86a5dbe5cab433be6f961c" attention-couple-ComfyUI "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/ExponentialML/ComfyUI_ELLA.git "303fe87f74c6237d05df8c17a06f2812e6028754" ComfyUI_ELLA "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/kijai/ComfyUI-KJNodes.git "0298e965d267977993715daa226fd2591d824ed8" ComfyUI-KJNodes "${TARGET_DIR}/custom_nodes"
+checkout_gitrepo https://github.com/theUpsider/ComfyUI-Logic.git "ba753246ba7f11ca935a68b16c5ad83317edc089" ComfyUI-Logic "${TARGET_DIR}/custom_nodes"
 
-checkout_gitrepo https://github.com/ArtBot2023/CharacterFaceSwap.git "f395496e5bf50d88579934ec2f776cd81866e8e9" CharacterFaceSwap ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/evanspearman/ComfyMath.git "be9beab9923ccf5c5e4132dc1653bcdfa773ed70" ComfyMath ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet.git "33d9884b76e8d7a2024691c5d98308e7e61bf38d" ComfyUI-Advanced-ControlNet ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved.git "f9e0343f4c4606ee6365a9af4a7e16118f1c45e1" ComfyUI-AnimateDiff-Evolved ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/meap158/ComfyUI-Background-Replacement.git "2a686281846f81366010caaf99afb78c81fab540" ComfyUI-Background-Replacement ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git "3f2c021e50be2fed3c9d1552ee8dcaae06ad1fe5" ComfyUI-Custom-Scripts ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/yolain/ComfyUI-Easy-Use.git "689d988130fd8191cd76f7b639d3640c31d12832" ComfyUI-Easy-Use ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/Fannovel16/ComfyUI-Frame-Interpolation.git "5e11679995c68f33891c306a393915feefe234b5" ComfyUI-Frame-Interpolation ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/kijai/ComfyUI-KJNodes.git "0d46ee88660419b5424eacbe6666ffc8a6a0f40f" ComfyUI-KJNodes ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/shiimizu/ComfyUI-TiledDiffusion.git "d3d783f63118e41b9d5d78ab13d5fcb7687314f1" ComfyUI-TiledDiffusion ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/pythongosssss/ComfyUI-WD14-Tagger.git "d2d482868364f942ace65d49630d017ec13ee47e" ComfyUI-WD14-Tagger ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/EllangoK/ComfyUI-post-processing-nodes.git "de7a29b339cd88586b54b171249b35785d20a23c" ComfyUI-post-processing-nodes ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/BlenderNeko/ComfyUI_ADV_CLIP_emb.git "c8b14087afab8efae22fce098b742f01cce22947" ComfyUI_ADV_CLIP_emb ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git "d78b780ae43fcf8c6b7c6505e6ffb4584281ceca" ComfyUI_Comfyroll_CustomNodes ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/chflame163/ComfyUI_LayerStyle.git "07c37fd72bb3c68d1e6bf215b8e8bc5c5b9320eb" ComfyUI_LayerStyle ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git "b303386bd363df16ad6706a13b3b47a1c2a1ea49" ComfyUI_UltimateSDUpscale ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes.git "2bb8d533b5cdc7bb409a2900e5318b4f546e9c2b" Derfuu_ComfyUI_ModdedNodes ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/djbielejeski/a-person-mask-generator.git "99709cf999bd00fe0eb864f55acabda88e3e2654" a-person-mask-generator ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/chrisgoringe/cg-use-everywhere.git "f9de27a62387be23a3b6398d45d18ec525f3b907" cg-use-everywhere ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/giriss/comfy-image-saver.git "65e6903eff274a50f8b5cd768f0f96baf37baea1" comfy-image-saver ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/sipherxyz/comfyui-art-venture.git "ccc620306266fe439bf14168f08d8121b19238ea" comfyui-art-venture ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/Layer-norm/comfyui-lama-remover.git "675a395ba60886a05cbb7d0aee9e9ddf5f2cb5f7" comfyui-lama-remover ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/Gourieff/comfyui-reactor-node.git "05bf228e623c8d7aa5a33d3a6f3103a990cfe09d" comfyui-reactor-node ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/jamesWalker55/comfyui-various.git "ec58185387c75b74ee93d6f835ff480772dbc64d" comfyui-various ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/storyicon/comfyui_segment_anything.git "ab6395596399d5048639cdab7e44ec9fae857a93" comfyui_segment_anything ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/jags111/efficiency-nodes-comfyui.git "3b7e89d969c02b80ff3662f34813679167b835fa" efficiency-nodes-comfyui ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/palant/image-resize-comfyui.git "ae5888637742ff1668b6cd32954ba48d81dbd39d" image-resize-comfyui ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/BadCafeCode/masquerade-nodes-comfyui.git "69a944969c29d1c63dfd62eb70a764bceb49473d" masquerade-nodes-comfyui ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/shadowcz007/comfyui-mixlab-nodes.git "c3679ac90b6736ad344cde482bddbe9dd58e31d9" comfyui-mixlab-nodes ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/rgthree/rgthree-comfy.git "79f5fea259cae29dd934d1eb7be378fee2777aa2" rgthree-comfy ${TARGET_DIR}/custom_nodes
-checkout_gitrepo https://github.com/cubiq/ComfyUI_essentials.git "78fa0fdc397760a1e4239d43011015d17f70291e" ComfyUI_essentials ${TARGET_DIR}/custom_nodes
 
 echo "Downloading input files..."
 # download_file 'https://photoshot-us.s3.amazonaws.com/comfy/logo-quick-qr-art_transparent_white_small.png' "${TARGET_DIR}/input/logo-quickqr.art-transparent.png"
